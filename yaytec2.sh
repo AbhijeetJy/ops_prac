f0717ba1ce195fac8df5fd080d107d230e84ac24
AbhijeetJy <112241315+AbhijeetJy@users.noreply.github.com>
2024-08-12 00:21:03 +0530

    Update yaytec2.sh

--- yaytec2.sh
+++ yaytec2.sh
@@ -4,7 +4,7 @@ sudo chmod 777 /var/run/docker.sock
 
 #setting environment variable for the current shell
 
-export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY AWS_DEFAULT_REGION=ap-south-1
+# export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY AWS_DEFAULT_REGION=ap-south-1
 
 aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/r3o8w1g0
 
